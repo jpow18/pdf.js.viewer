137db8cf4dcd498fa2bc8a023ec42af65e0229dd
jpow18 <jpow@insuranceautomationgroup.com>
2024-06-06 13:14:04 -0400

    feat: added postRenderHook to apply arbitrary hooks. Changed savePageData to avoid clashing with any hooks that are attached. Changed some Ajax. Improved page navigation to prevent reduntant reload of current page.

--- dist/PDFJsViewer.js
+++ dist/PDFJsViewer.js
@@ -94467,6 +94467,7 @@ class PDFJsViewer {
         this.loadedDoc = null;
         this.numPages = null;
         this.currentPageNumber = null;
+        this.previousPageNumber = 1;
         this.formData = {};
         this.formRenderingOptions = {};
         this.width = null;
@@ -94474,10 +94475,16 @@ class PDFJsViewer {
         this.annotations = null;
         this.saving = false;
         this._postRenderHook = null;
+        this.dirty = false;
+        this.numberedPageNavigation = false;
     }
 
     getCurrentPageNumber() {
         return this.currentPageNumber;
+    }
+
+    getDirty() {
+        return this.dirty;
     }
 
     async getFormValues() {
@@ -94490,6 +94497,10 @@ class PDFJsViewer {
 
     getNumberOfPages() {
         return this.PDFViewer.pagesCount;
+    }
+
+    getPreviousPageNumber() {
+        return this.previousPageNumber;
     }
   
     async loadDocument(url) {
@@ -94523,6 +94534,12 @@ class PDFJsViewer {
 
     async loadPage(pageNumber) {
         try {
+            if (pageNumber == this.previousPageNumber)
+            {
+                return;
+            }
+
+
             this.savePageData();
             var width = this.width;
             let height = this.height;
@@ -94544,7 +94561,15 @@ class PDFJsViewer {
             this.loadedDoc.getPage(pageNumber).then(function() {
                 this.render(width, height, this.pdfUrl, false, pageNumber, this.formData, this.formRenderingOptions);
                 this.loaderEnd();
-                this.currentPage = pageNumber;
+
+                if (this.numberedPageNavigation) {
+                    this.previousPageNumber = pageNumber;
+                }
+                else {
+                    this.previousPageNumber = this.currentPageNumber;
+                }
+                this.numberedPageNavigation = true;
+                this.currentPageNumber = pageNumber;
             }.bind(this));
         } catch (e) {
             alert(e.message);
@@ -94593,24 +94618,24 @@ class PDFJsViewer {
     }
 
     navigateToNextPage() {
-        let currentPageNumber = this.getCurrentPageNumber();
-        const pagesCount = this.numPages;
-        if (currentPageNumber >= pagesCount) {
-            return currentPageNumber;
+        if (this.currentPageNumber >= this.numPages) {
+            return this.currentPageNumber;
         }
+        this.numberedPageNavigation = false;
 
+        this.previousPageNumber = this.currentPageNumber;
         this.currentPageNumber++;
-        return this.getCurrentPageNumber();
+        return this.currentPageNumber;
     }
 
     navigateToPreviousPage() {
-        let currentPageNumber = this.getCurrentPageNumber();
-        if (currentPageNumber <= 1) {
-            return currentPageNumber;
+        if (this.currentPageNumber <= 1) {
+            return this.currentPageNumber;
         }
-
+        this.numberedPageNavigation = false;
+        this.previousPageNumber = this.currentPageNumber;
         this.currentPageNumber--;
-        return this.getCurrentPageNumber();
+        return this.currentPageNumber;
     }
 
     async render(width = false, height = false, pdfUrl, pdfDataUrl, pageNumber = 1, values = {}, formRenderingOptions  = {}) {
@@ -94699,9 +94724,11 @@ class PDFJsViewer {
 
             this.loaderEnd();
             this.PDFPageView.draw().then(() => {
+
                 if (this._postRenderHook) {
                     this._postRenderHook();
                 }
+
             });
 
 
@@ -94710,26 +94737,22 @@ class PDFJsViewer {
         }
     }
 
-    async savePageData() {
-        try {
-            const tempData = await this.getFormValues();
-            for (const prop in tempData) {
-                let foundKey = null;
-                this.annotations.forEach((annotation) => {
-                    if (Object.values(annotation).includes(prop)) {
-                        foundKey = annotation.fieldName;
-                    }
-                });
-                this.formData[foundKey] = tempData[prop]["value"];
-            }
-        } catch (e) {
-            alert(e.message);
+    savePageData() {
+        const inputElements = jQuery('[data-element-id]');
+        const tempData = {};
+        inputElements.each(function() {
+            let id = jQuery(this).attr('id');
+            let value = jQuery(this).val();
+            tempData[id] = value;
+        });
+        for (const prop in tempData) {
+            this.formData[prop] = tempData[prop];
         }
     }
 
     async savePdfData(saveUrl, token = false) {
         try {
-            await this.savePageData();
+            this.savePageData();
             if (this.saving) {
                 alert('Already processing request.');
                 return false;
@@ -94745,34 +94768,37 @@ class PDFJsViewer {
                 postData.csrf = token;
             }
             this.loaderStart();
-            jQuery.ajax({
-                dataType: "json",
-                url: saveUrl,
-                async: false,
-                data: postData,
-                method: "POST",
-                context: this,
-                success: function(results) {
-                    try {
-                        if (results.success) {
-                            success = results;
-                        }
-                        else {
-                            if (typeof results.message != 'undefined') {
-                                console.error(results.message);
+            success = await new Promise((resolve, reject) => {
+                jQuery.ajax({
+                    dataType: "json",
+                    url: saveUrl,
+                    data: postData,
+                    method: "POST",
+                    context: this,
+                    success: function(results) {
+                        try {
+                            if (results.success) {
+                                resolve(results);
+                            } else {
+                                if (typeof results.message !== 'undefined') {
+                                    console.error(results.message);
+                                }
+                                resolve(false);
                             }
+                        } catch (e) {
+                            console.error(e);
+                            resolve(false);
                         }
+                    },
+                    error: function(jqXHR, textStatus, errorThrown) {
+                        console.error('AJAX error: ', textStatus, errorThrown);
+                        resolve(false);
+                    },
+                    complete: function() {
+                        this.saving = false;
+                        this.loaderEnd();
                     }
-                    catch (e) {}
-                },
-                fail: function() {
-                    this.loaderEnd();
-                },
-                complete: function() {
-                    this.saving = false;
-                    // Schedule the next request when the current one's complete
-                    this.loaderEnd();
-                }
+                });
             });
             return success;
         }
@@ -94781,6 +94807,10 @@ class PDFJsViewer {
             alert(e.message);
         }
         return false;
+    }
+
+    setDirty() {
+        this.dirty = true;
     }
 
     setPostRenderHook(hook) {
